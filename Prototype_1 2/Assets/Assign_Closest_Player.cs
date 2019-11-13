58aad88412e454d98ead16a21e17cf1c7f0febae
Unknown <cmr648@nyu.edu>
2019-11-13 11:46:32 -0500

    First_Commit_Backup_Important

---
+++ Prototype_1 2/Assets/Assign_Closest_Player.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Assign_Closest_Player : MonoBehaviour
+{
+    public GameObject Closest_Enemy;
+
+    public GameObject[] Players;
+
+    public bool Holding_Trophy;
+
+    public GameObject Trophy;
+
+    [SerializeField]
+    public AIDestinationSetter My_Destition_Setter;
+    public AIPath My_AI_PATH;
+
+    public Enemy_Brain My_Enemy_Brain;
+
+    public float Speed_Yes_Trophy;
+    public float Speed_No_Trophy;
+
+    public float Bullet_Rate_With_Trophy;
+    public float Bullet_Rate_Without_Trophy;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Players = GameObject.FindGameObjectsWithTag("Player");
+        My_Destition_Setter = GetComponent<AIDestinationSetter>();
+        My_AI_PATH = GetComponent<AIPath>();
+
+        Trophy = GameObject.FindGameObjectWithTag("Trophy");
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Holding_Trophy = Trophy.GetComponent<Trophy_Timer>().Holding_Trophy;
+
+        Look();
+
+        if (Holding_Trophy == true)
+        {
+            My_Destition_Setter.target = Trophy.transform.parent;
+            My_AI_PATH.speed = Speed_Yes_Trophy;
+            My_Enemy_Brain.Seconds_In_Between = Bullet_Rate_With_Trophy;
+
+        }
+        else
+        {
+            My_Destition_Setter.target = Closest_Enemy.transform;
+            My_AI_PATH.speed = Speed_No_Trophy;
+            My_Enemy_Brain.Seconds_In_Between = Bullet_Rate_Without_Trophy;
+        }
+
+       
+
+
+
+    }
+
+    void Look()
+    {
+        // for greatest distance
+        float Distance_To_Closest = Mathf.Infinity;
+
+
+
+        foreach (GameObject Next_Player in Players)
+        {
+            float Distance_To_Player = (Next_Player.transform.position - transform.position).sqrMagnitude;
+
+            if (Next_Player.GetComponent<Player_Health>().Player_Alive == true)
+            {
+                if (Distance_To_Player < Distance_To_Closest)
+                {
+                    Distance_To_Closest = Distance_To_Player;
+                    Closest_Enemy = Next_Player;
+                }
+            }
+
+
+        }
+
+        Vector3 dir = Closest_Enemy.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+
+    }
+}
---
+++ Prototype_1 2/Assets/Win_Conditions.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Win_Conditions : MonoBehaviour
+{
+    public bool Boss_Defeated;
+    public bool Score_Cap_Reached;
+    public bool Tresure_Held;
+
+    public GameObject Boss;
+
+    public Treasure_Points MY_Treasurepoints;
+    public int Max_Score;
+
+    public Trophy_Timer My_Trophy_Timer;
+
+    public Text Win_Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Win_Text.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Boss.gameObject == null)
+        {
+            Boss_Defeated = true;
+        }
+
+        if(MY_Treasurepoints.Current_Score >= Max_Score)
+        {
+            Score_Cap_Reached = true;
+        }
+
+        if(My_Trophy_Timer.Current_Time_Amount <= 0)
+        {
+            Tresure_Held = true;
+        }
+
+        if(Boss_Defeated == true || Score_Cap_Reached == true || Tresure_Held == true)
+        {
+            Win_Text.enabled = true;
+            Time.timeScale = 0;
+        }
+
+    }
+}
